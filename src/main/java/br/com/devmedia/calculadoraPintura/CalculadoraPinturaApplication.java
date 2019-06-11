49bc14b18fe9f0059de9bc5f7a74925c8fec233e
Rodrigo-Vellozo <rodrigo@mvpdevup.com.br>
2019-06-11 17:06:08 -0300

    :mega: primeiro commit!

---
+++ src/main/java/br/com/devmedia/calculadoraPintura/CalculadoraPinturaApplication.java
@@ -0,0 +1,13 @@
+package br.com.devmedia.calculadoraPintura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraPinturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraPinturaApplication.class, args);
+	}
+
+}
