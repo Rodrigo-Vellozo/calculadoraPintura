11c4d96721d3880a4b77de6a13dda50b5094dfa1
Rodrigo-Vellozo <rodrigo@mvpdevup.com.br>
2019-06-13 18:27:35 -0300

    :walking: reprovado...

--- src/main/java/br/com/devmedia/calculadoraPintura/controller/CalculadoraController.java
+++ src/main/java/br/com/devmedia/calculadoraPintura/controller/CalculadoraController.java
@@ -9,21 +9,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class CalculadoraController {
 
-    private Comodo comodo;
-
-
-
-    @GetMapping
+    @GetMapping("/")
     public String index(@ModelAttribute("comodo") Comodo calculadora){
         return "index";
     }
 
-    @PostMapping(value = "/calcular")
+    @PostMapping("/calcular")
     public ModelAndView calcular(@ModelAttribute("comodo") Comodo comodo){
         ModelAndView modelAndView = new ModelAndView("listagem");
 
         CalculadoraDeLitragemDeTinta calculadoraDeLitragemDeTinta = new CalculadoraDeLitragemDeTinta();
-        System.out.println(calculadoraDeLitragemDeTinta.calcularLitragem(comodo));
 
         modelAndView.addObject("litros", calculadoraDeLitragemDeTinta.calcularLitragem(comodo));
 
--- src/main/java/br/com/devmedia/calculadoraPintura/model/CalculadoraDeLitragemDeTinta.java
+++ src/main/java/br/com/devmedia/calculadoraPintura/model/CalculadoraDeLitragemDeTinta.java
@@ -9,17 +9,7 @@ public class CalculadoraDeLitragemDeTinta {
     public Double calcularLitragem(Comodo comodo){
         areaParedes = 2 *(comodo.getLargura()+comodo.getProfundidade())*comodo.getAltura();
         areaTeto = comodo.getLargura() * comodo.getProfundidade();
-        return (areaParedes+areaTeto)/2;
+        return (areaParedes+areaTeto)/rendimento;
     }
 
-    /*
-    public Double calcularAreaParedes(Comodo comodo){
-        areaParedes = 2 *(comodo.getLargura()+comodo.getProfundidade())*comodo.getAltura();
-        return areaParedes;
-    }
-    public Double calcularAreaTeto(Comodo comodo){
-        areaTeto = comodo.getLargura() * comodo.getProfundidade();
-        return areaTeto;
-    }
-    */
 }
