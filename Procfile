2dcb182bd73a90d1b154cc6fab718a07b7d5dbf5
Rodrigo-Vellozo <rodrigo@mvpdevup.com.br>
2019-06-14 08:59:10 -0300

    :mega: remocao do arquivo procfile

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar -Dserver.port=$PORT $JAVA_OPTS target/*.jar
--- src/main/java/br/com/devmedia/calculadoraPintura/CalculadoraPinturaApplication.java
+++ src/main/java/br/com/devmedia/calculadoraPintura/CalculadoraPinturaApplication.java
@@ -2,9 +2,6 @@ package br.com.devmedia.calculadoraPintura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @SpringBootApplication
 public class CalculadoraPinturaApplication{
