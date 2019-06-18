74ab860ad5b36ba38e174e8f8f5895178b1a5128
Rodrigo-Vellozo <rodrigo@mvpdevup.com.br>
2019-06-18 10:48:12 -0300

    :mega:

--- src/main/java/br/com/devmedia/calculadoraPintura/controller/ComodoController.java
+++ src/main/java/br/com/devmedia/calculadoraPintura/controller/ComodoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class CalculadoraController {
+public class ComodoController {
 
     @GetMapping("/")
     public String index(@ModelAttribute("comodo") Comodo calculadora){
@@ -16,7 +16,7 @@ public class CalculadoraController {
 
     @PostMapping("/calcular")
     public ModelAndView calcular(@ModelAttribute("comodo") Comodo comodo){
-        ModelAndView modelAndView = new ModelAndView("listagem");
+        ModelAndView modelAndView = new ModelAndView("quantidadeLitros");
 
         CalculadoraDeLitragemDeTinta calculadoraDeLitragemDeTinta = new CalculadoraDeLitragemDeTinta();
 
